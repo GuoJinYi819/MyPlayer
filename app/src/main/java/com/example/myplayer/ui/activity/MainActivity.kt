03a937a8a67df556335fecc934a8497d86067f7d
GuoJinYi819 <gjy2000@>
2020-05-12 21:42:01 +0800

    05-12：ToolBar设置

--- app/src/main/java/com/example/myplayer/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myplayer/ui/activity/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.myplayer.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.BaseAdapter
+import androidx.appcompat.widget.Toolbar
 import com.example.myplayer.R
 import com.example.myplayer.base.BaseActivity
+import com.example.myplayer.util.ToolBarManager
+import org.jetbrains.anko.find
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(),ToolBarManager{
+    override val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
+
     override fun initLayoutId(): Int {
         return R.layout.activity_main
+    }
+
+    override fun initData() {
+        initMainToolBar()
     }
 
 }
---
+++ app/src/main/java/com/example/myplayer/util/ToolBarManager.kt
@@ -0,0 +1,43 @@
+package com.example.myplayer.util
+
+import android.content.Intent
+import android.os.Build
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import com.example.myplayer.R
+import com.example.myplayer.ui.activity.SettingActivity
+import java.util.*
+
+
+/**ClassName: MyPlayer
+ * @author 作者 : GuoJinYi
+ * @version 创建时间：2020/5/12 0012 21:01
+ * @Description: 用途：完成特定功能
+ */
+interface ToolBarManager {
+    val toolbar: Toolbar
+    //初始化 主界面的toolbar
+    fun initMainToolBar(){
+        toolbar.setTitle("我的影音")
+        toolbar.inflateMenu(R.menu.main)
+
+        //如果java接口中 只有一个未实现的对象 可以直接省略接口对象 直接{ }表示未实现的方法
+//        toolbar.setOnMenuItemClickListener{
+//
+//            true
+//        }
+        //与上边作用相同
+        toolbar.setOnMenuItemClickListener(object :Toolbar.OnMenuItemClickListener{
+            override fun onMenuItemClick(item: MenuItem?): Boolean {
+                when(item?.itemId){
+                    R.id.setting->{
+                        //跳转到指定界面
+                        toolbar.context.startActivity(Intent(toolbar.context,SettingActivity::class.java))
+                    }
+                }
+                return true
+            }
+        })
+    }
+}
